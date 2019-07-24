057af26df369c35ba6bcbef39ca663d0589dc16a
Ciaran Quinlan <ciaran.quinlan@gmail.com>
2019-07-24 13:30:28 +0100

    flask, mongo and heroku all talking to each other

--- connectmongo.py
+++ connectmongo.py
@@ -30,6 +30,6 @@ def get_tasks():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             # to run in heroku uncomment this line and comment the port=5000 line
-            port=int(os.environ.get('PORT')),
-            # port=5000,
+            # port=int(os.environ.get('PORT')),
+            port=5000,
             debug=True)
