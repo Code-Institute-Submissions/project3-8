8caf7774b354bb10144b32f29afc235c4d152d74
Ciaran Quinlan <ciaran.quinlan@gmail.com>
2019-07-26 10:14:08 +0100

    Quote status working for adding and deleting.

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
 # README.md
 This is the start of my project.
-I have done some bits already as I was doing test tasks while I was following
- the videos and doing the mini projects.
-
+I have done some bits already as I was doing test tasks while I was followingthe videos and doing the mini projects.
 Some git commits will be quick as a result.
+
+
+# Project Purpose
+The purpose of this project is to deliver a website that showcases the technology we have been learning and to incorporate a form that allows customers specify a brief for a full stack development and an admin back end to reply and edit the quotes.
+The 'CRM' system will be a number of forms that the customer can enter a brief for a full stack development project.
+The system will also have a back end to view ,edit and respond to the briefs recieved.
+
+- UX design
+- Suitability for purpose
+- Navigation
+- Ease of Use
+- Information Architecture
+- Responsive Design
+- Image Presentation
+- Colour scheme and typography
+- Appropriate use of HTML
+- Appropriate use of CSS
+- Directory Structure and File Naming
+- Version Control
+I am using GITHB for version control and do a commit in the morning and afternoon or after major features are added.
+
+
+- Testing implementation
+- Testing write-up
+- Readme file
+- Comments
+- Deployment implementation
+- Deployment write-up
+
+
+
+
+
+
 
 ## GIT 
 [GitHub repo is here](https://github.com/ciaranq/project3)
--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 #! /usr/local/bin/python
 
-
 # Flask locally needs this above line to run as well as mods to the port number
 # this is the test script to test connection to mongo connectmongo.py
-
 
 import os
 from flask import Flask, render_template, redirect, request, url_for
@@ -29,12 +27,15 @@ def get_quotes():
 # This function displays the edit page and populates it with the document selected in quote.html
 @app.route('/edit_quote/<quote_id>')
 def edit_quote(quote_id):
-        Find the quote based on the unique quote_id
+    # Find the quote based on the unique quote_id
     the_quote = mongo.db.quote.find_one({'_id': ObjectId(quote_id)})
-    all_status = mongo.db.status.find()
+    all_status = mongo.db.status.find({})
+    # nextquote = mongo.db.quotenumber.find()
+    # print(nextquote)
+    # print(all_status)
+    # print(the_quote)
     return render_template(
         'editquote.html', quote=the_quote, statuses=all_status)
-
 
 
 @app.route('/add_task')
@@ -49,18 +50,19 @@ def insert_task():
     return redirect(url_for('get_tasks'))
 
 
-
-
 @app.route('/update_quote/<quote_id>', methods=['POST'])
 def update_quote(quote_id):
+    # This function is losing the existing data so i must figure out how to change just the things we need
+    # need to use updae $set and specify excactly tyhe ones i updayinfg
     mongo.db.quote.update(
         {'_id': ObjectId(quote_id)},
         {
-            'quote_name': request.form.get('quote_name'),
-            'category_name': request.form.get('category_name'),
-            'quote_description': request.form.get('quote_description'),
-            'due_date': request.form.get('due_date'),
-            'is_urgent': request.form.get('is_urgent'),
+            'quoteId': 44,
+            'name': request.form.get('quote_name'),
+            'brief': request.form.get('quote_brief'),
+            'quoteStatus': request.form.get('quote_status'),
+            'liveDate': request.form.get('live_date'),
+            'isurgent': request.form.get('is_urgent'),
         })
     return redirect(url_for('get_quotes'))
 
@@ -72,49 +74,58 @@ def delete_quote(quote_id):
     # return redirect(url_for('get_tasks'))
 
 
-@app.route('/get_categories')
-def get_categories():
-    # return render_template('categories.html',
-    #                        categories=mongo.db.categories.find())
-    return 'Hello World ...category  '
+# Status decorators and functions
+# This section deals with the status collection of document in crm.status
+
+@app.route('/get_status')
+def get_status():
+    # this is to list the quote status
+    return render_template('getstatus.html', statuses=mongo.db.status.find())
+    # return 'Hello World ...status '
 
 
-@app.route('/edit_category/<category_id>')
-def edit_category(category_id):
-    # the_category = mongo.db.categories.find_one(
-    #     {'_id': ObjectId(category_id)})
-    # return render_template('editcategory.html', category=the_category)
-    return 'Hello World ...category  '
+@app.route('/delete_status/<status_id>', methods=['POST'])
+def delete_status(status_id):
+    # Deleting based on the document selected
+    mongo.db.status.remove({'_id': ObjectId(status_id)})
+    return redirect(url_for('get_status'))
 
 
-@app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id):
-    # mongo.db.categories.update(
-    #     {'_id': ObjectId(category_id)},
-    #     {'category_name': request.form.get('category_name')})
-    # return redirect(url_for('get_categories'))
-    return 'Hello World ...category  '
+@app.route('/edit_status/<status_id>')
+def edit_status(status_id):
+    # This find the selected document from the collection
+    the_status = mongo.db.status.find_one(
+        {'_id': ObjectId(status_id)})
+    return render_template('editstatus.html', status=the_status)
 
 
-@app.route('/delete_category/<category_id>', methods=['POST'])
-def delete_category(category_id):
-    # mongo.db.categories.remove({'_id': ObjectId(category_id)})
-    # return redirect(url_for('get_categories'))
-    return 'Hello World ...category  '
+@app.route('/update_status/<status_id>', methods=['POST'])
+def update_status(status_id):
+        # I want to make sure its always in upper case
+    # I tried using |upper in jinja but it was not updating on the page
+    quotestatus = request.form.get('quote_status')
+    quotestatusupper = quotestatus.upper()
+    mongo.db.status.update(
+        {'_id': ObjectId(status_id)},
+        {'quote_status': quotestatusupper})
+    return redirect(url_for('get_status'))
 
 
-@app.route('/insert_category', methods=['POST'])
-def insert_category():
-    # category_doc = {'category_name': request.form.get('category_name')}
-    # mongo.db.categories.insert_one(category_doc)
-    # return redirect(url_for('get_categories'))
-    return 'Hello World ...category  '
+@app.route('/insert_status', methods=['POST'])
+def insert_status():
+    # I want to make sure its always in upper case
+    # I tried using |upper in jinja but it was not updating on the page
+    quotestatus = request.form.get('quote_status')
+    quotestatusupper = quotestatus.upper()
+    status_doc = {'quote_status': quotestatusupper}
+    mongo.db.status.insert_one(status_doc)
+    return redirect(url_for('get_status'))
 
 
-@app.route('/add_category')
-def add_category():
-    # return render_template('addcategory.html')
-    return 'Hello World ...category  '
+@app.route('/add_status')
+def add_status():
+    # adds new status on the addstatus page
+    return render_template('addstatus.html')
 
 
 # this route was used to test and setup the connection
